c35c9cb2f9ea0c1a0819509caf24077875e4fc9e
Vonglory176 <SkylerGConley@gmail.com>
2024-08-15 17:38:00 -0700

    Added info, transcript and translation types

--- src/utils/types.d.ts
+++ src/utils/types.d.ts
@@ -8,8 +8,26 @@ interface FileDisplayProps {
     file: File | null
     audioStream: MediaStream | null
     handleResetAudio: () => void
+    handleFormSubmission: () => void
 }
 
 interface TranscribingProps {
     downloding: boolean
+}
+
+interface InformationProps {
+    output: { text: string }[],
+    finished: boolean
+}
+
+interface TranscriptionProps {
+    textElement: string | string[]
+}
+
+interface TranslationProps {
+    textElement: string
+    toLanguage: string
+    translating: boolean
+    handleTranslate: () => void
+    setToLanguage: (toLanguage: string) => void
 }
