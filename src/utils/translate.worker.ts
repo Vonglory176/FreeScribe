18e124f869896b7091182a003fc845d55091d754
Vonglory176 <SkylerGConley@gmail.com>
2024-08-17 13:46:37 -0700

    Fixed some typescript issues

--- src/utils/translate.worker.ts
+++ src/utils/translate.worker.ts
@@ -5,9 +5,9 @@ env.allowLocalModels = false
 class MyTranslationPipeline {
     static task: PipelineType = 'translation'
     static model: any = 'Xenova/nllb-200-distilled-600M'
-    static instance: any = undefined
+    static instance: any | undefined = undefined
 
-    static async getInstance(progress_callback = undefined) {
+    static async getInstance(progress_callback?: (x: any) => void) {
         if (this.instance === undefined) {
             this.instance = pipeline(this.task, undefined, { progress_callback })
         }
@@ -20,12 +20,14 @@ self.addEventListener('message', async (event) => {
     let translator = await MyTranslationPipeline.getInstance(x => {
         self.postMessage(x)
     })
+
     console.log(event.data)
+
     let output = await translator(event.data.text, {
         tgt_lang: event.data.tgt_lang,
         src_lang: event.data.src_lang,
 
-        callback_function: x => {
+        callback_function: (x: any) => {
             self.postMessage({
                 status: 'update',
                 output: translator.tokenizer.decode(x[0].output_token_ids, { skip_special_tokens: true })
@@ -33,7 +35,7 @@ self.addEventListener('message', async (event) => {
         }
     })
 
-    console.log('HEHEHHERERE', output)
+    // console.log('HEHEHHERERE', output)
 
     self.postMessage({
         status: 'complete',
