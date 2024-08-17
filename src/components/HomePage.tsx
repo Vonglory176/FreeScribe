316e07abe3bfe5ca0f996ff41e7c2a86daf129a4
Vonglory176 <SkylerGConley@gmail.com>
2024-08-17 13:46:09 -0700

    Changed the subheader to remove "translate"

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -84,9 +84,10 @@ const HomePage: React.FC<HomePageProps> = ({setFile, setAudioStream}) => { {/* ,
   return (
     <main className="flex-1 p-4 flex flex-col gap-3 sm:gap-4 text-center justify-center pb-20">
         <h1 className='font-semibold text-5xl sm:text-6xl md:text-7xl'>Free<span className='text-blue-400 bold'>Scribe</span></h1>
-        <h3 className='font-medium'>Record <span 
+        {/* <h3 className='font-medium'>Record <span 
         className='text-blue-400'>&rarr;</span> Transcribe <span
-        className='text-blue-400'>&rarr;</span> Translate</h3>
+        className='text-blue-400'>&rarr;</span> Translate</h3> */}
+        <h3 className='text-lg'>Easily record and transcribe your audio</h3>
 
         {/* Record button */}
         <button 
